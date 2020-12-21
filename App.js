d51d024803043ec94098284443c09610f2fd5a6c
OmegaP1 <j.veloso.00@gmail.com>
2020-12-21 13:04:41 +0000

    Password view

--- App.js
+++ App.js
@@ -1,122 +1,188 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View ,TextInput,TouchableOpacity,ImageBackground,Image } from 'react-native';
-import gradient from './assets/gradientv2.png';
-import {BiLockOpenAlt,BiMobileAlt} from "react-icons/bi";
-import logo from './assets/logo_skaphandrus.png';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import gradient from "./assets/gradientv2.png";
+import {
+  BiLockOpenAlt,
+  BiMobileAlt,
+  BiUserCircle,
+  BiLowVision,
+} from "react-icons/bi";
+import logo from "./assets/logo_skaphandrus.png";
 
-export default function App() {
+const App = () => {
+  const [data, setData] = React.useState({
+    username: "",
+    password: "",
+    check_textInputChange: false,
+    secureTextEntry: true,
+    isValidUser: true,
+    isValidPassword: true,
+  });
+
+  const handlePasswordChange = (val) => {
+    if (data.secureTextEntry == true) {
+      setData({
+        ...data,
+        password: val,
+        secureTextEntry: false,
+      });
+    } else {
+      setData({
+        ...data,
+        password: val,
+        secureTextEntry: true,
+      });
+    }
+  };
+
   return (
-
     <View style={styles.container}>
       <ImageBackground source={gradient} style={styles.image}>
-        <Text style={styles.title}>
-          Skaphandrus
-        </Text>       
+        <Text style={styles.title}>Skaphandrus</Text>
 
-          <Image 
-          style={styles.logo}
-          source={logo}
+        <Image style={styles.logo} source={logo} />
+
+        <View style={styles.logContainer}>
+          <View style={styles.iconContainer}>
+            <BiUserCircle style={styles.icon} />
+          </View>
+          <TextInput placeholder="Username" style={styles.input} />
+        </View>
+
+        <View style={styles.logContainer}>
+          <View style={styles.iconContainer}>
+            <BiLockOpenAlt style={styles.icon} />
+          </View>
+          <TextInput
+            placeholder="Password"
+            style={styles.input}
+            secureTextEntry={data.secureTextEntry}
           />
+          <TouchableOpacity onPress={handlePasswordChange}>
+            <BiLowVision
+              style={styles.icon}
+              onChangeText={(val) => textInputChange(val)}
+            />
+          </TouchableOpacity>
+        </View>
 
         <View style={styles.logContainer}>
-          <View style={styles.iconContainer}>        
-            <BiMobileAlt style={styles.icon}/>  
+          <View style={styles.iconContainer}>
+            <BiLockOpenAlt style={styles.icon} />
           </View>
-        <TextInput  placeholder="username"  style ={styles.input}/>
-        </View>  
+          <TextInput
+            placeholder="Confirm Password"
+            style={styles.input}
+            secureTextEntry={data.secureTextEntry}
+          />
+          <TouchableOpacity onPress={handlePasswordChange}>
+            <BiLowVision
+              style={styles.icon}
+              onChangeText={(val) => textInputChange(val)}
+            />
+          </TouchableOpacity>
+        </View>
 
         <View style={styles.logContainer}>
-          <View style={styles.iconContainer}>        
-            <BiLockOpenAlt style={styles.icon}/>  
+          <View style={styles.iconContainer}>
+            <BiMobileAlt style={styles.icon} />
           </View>
-        <TextInput placeholder="password"  style ={styles.input}/>
-        </View>            
-        
-          <View style={styles.btnContainer}>        
-            <TouchableOpacity style={styles.btn}>
-                <Text style={styles.btnText}>LogIn</Text>
-            </TouchableOpacity>
-            <Text>Don't have an account? SingIn</Text>
-            <Text>Forgot your password?</Text>
-            
-          </View>
-        </ImageBackground>
+          <TextInput placeholder="Email" style={styles.input} />
+        </View>
+
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Login</Text>
+          </TouchableOpacity>
+          <Text>Forgot your password?</Text>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Register</Text>
+          </TouchableOpacity>
+          <Text>Don't have an account? SingIn</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: { 
+  container: {
     flex: 1,
     //backgroundColor:'#1af394',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
-    flex: 1,  
-    width:'100%',
+    flex: 1,
+    width: "100%",
     justifyContent: "center",
-    alignItems: 'center',
+    alignItems: "center",
   },
-  title :{
-    padding :20,   
-    fontSize:50,
-    color:'#fff' ,
-    position:'absolute',
-    top:10,
-    fontFamily:'New Times Roman',
+  title: {
+    padding: 20,
+    fontSize: 50,
+    color: "#fff",
+    position: "absolute",
+    top: 10,
+    fontFamily: "New Times Roman",
     //fontWeight:'bold',
   },
-  logo:{
+  logo: {
     //backgroundColor:'black',
-    width:200,
-    height:200,
-    alignItems: 'top',
-    justifyContent: 'top',   
+    width: 100,
+    height: 100,
+    alignItems: "top",
+    justifyContent: "top",
   },
-  logContainer:{
-    width:'80%',
-    alignItems: 'center',
-    flexDirection:'row',
-    justifyContent: 'center',  
-    margin:10, 
-    minHeight:50
+  logContainer: {
+    width: "80%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    margin: 10,
+    minHeight: 50,
   },
-  iconContainer:{
-    color:'white',
+  iconContainer: {
+    color: "white",
   },
-  icon:{
-   
+  icon: {},
+  input: {
+    borderBottomColor: "white",
+    borderBottomWidth: 2,
+    width: "100%",
+    padding: 10,
+    placeholderTextColor: "#fff",
+    textShadowColor: "#fff",
   },
-  input:{
-    borderBottomColor: 'white',
-    borderBottomWidth: 2, 
-    width:'100%',
-    padding:10,
-    placeholderTextColor:"#fff", 
-    textShadowColor:'#fff',
+  btnContainer: {
+    width: "80%",
+    alignItems: "center",
   },
-  btnContainer:{
-    width:'80%',
-    alignItems:'center',
-  },
-  btn:{
-    backgroundColor: '', /* Green */
-    borderColor: 'white',
-    borderRadius:10,
-    paddingVertical:5,
-    paddingHorizontal:5,
+  btn: {
+    backgroundColor: "" /* Green */,
+    borderColor: "white",
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 5,
     borderWidth: 3,
-    width:'80%',
-    minHeight:30,
-    marginTop:30,
-    },
-  btnText:{
-    color:'white',
-    textAlign:'center',
-    fontSize:20,
-    fontWeight:'bold',
-  
-  }
+    width: "80%",
+    minHeight: 30,
+    marginTop: 30,
+  },
+  btnText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 });
+export default App;
